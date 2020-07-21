01b49ba8ba6abb4e3f81c75ea9e7d5967f0038ad
oouuyab <01073785645@naver.com>
2020-07-21 11:42:46 +0900

    :heavy_plus_sign: Add 1931_meetingroom.js

---
+++ greedy/1931_meetingroom.js
@@ -0,0 +1,66 @@
+/*
+회의실배정 성공분류
+시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
+2 초	128 MB	48667	13738	10213	28.404%
+문제
+한 개의 회의실이 있는데 이를 사용하고자 하는 N개의 회의에 대하여 회의실 사용표를 만들려고 한다. 각 회의 I에 대해 시작시간과 끝나는 시간이 주어져 있고, 각 회의가 겹치지 않게 하면서 회의실을 사용할 수 있는 회의의 최대 개수를 찾아보자. 단, 회의는 한번 시작하면 중간에 중단될 수 없으며 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다. 회의의 시작시간과 끝나는 시간이 같을 수도 있다. 이 경우에는 시작하자마자 끝나는 것으로 생각하면 된다.
+
+입력
+첫째 줄에 회의의 수 N(1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N+1 줄까지 각 회의의 정보가 주어지는데 이것은 공백을 사이에 두고 회의의 시작시간과 끝나는 시간이 주어진다. 시작 시간과 끝나는 시간은 231-1보다 작거나 같은 자연수 또는 0이다.
+
+출력
+첫째 줄에 최대 사용할 수 있는 회의의 최대 개수를 출력한다.
+
+예제 입력 1 
+11
+1 4
+3 5
+0 6
+5 7
+3 8
+5 9
+6 10
+8 11
+8 12
+2 13
+12 14
+예제 출력 1 
+4
+힌트
+(1,4), (5,7), (8,11), (12,14) 를 이용할 수 있다.
+*/
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input = [];
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  let meetingIdx = parseInt(input[0]);
+  let meeting = input
+    .splice(1)
+    .map((el) => el.split(" ").map((el) => parseInt(el)))
+    .sort((a, b) => {
+      if (a[1] !== b[1]) {
+        return a[1] - b[1];
+      } else {
+        return a[0] - b[0];
+      }
+    });
+
+  let count = 1;
+  let schedule = [meeting[0]];
+
+  for (let i = 1; i < meetingIdx; i++) {
+    if (meeting[i][0] >= schedule[schedule.length - 1][1]) {
+      schedule.push(meeting[i]);
+      count++;
+    }
+  }
+
+  console.log(count);
+  process.exit();
+});
