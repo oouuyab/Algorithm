ee762ca7dd1a8e025b03c351e0bb8d4eb3aa477c
oouuyab <01073785645@naver.com>
2020-08-07 23:54:38 +0900

    :heavy_plus_sign: Add ReverseString And ReverseInteger.js

---
+++ LeetCode/Easy Collection/Strings/ReverseInteger.js
@@ -0,0 +1,46 @@
+/*
+Reverse Integer
+
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+*/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+    let numToStr = String(x);
+    let resule;
+    console.log(numToStr);
+    if (x < 0) {
+        let newX = numToStr.slice(1);
+        result = Number(makeReverseStr(newX)) * -1;
+    } else {
+        result = Number(makeReverseStr(numToStr));
+    }
+    if (result > Math.pow(2, 31) - 1 || result < Math.pow(2, 31) * -1) return 0;
+    return result;
+};
+
+let makeReverseStr = (numToStr) => {
+    let reverseStr = "";
+    for (let i = numToStr.length - 1; i >= 0; i--) {
+        reverseStr += numToStr[i];
+    }
+    return reverseStr;
+};
