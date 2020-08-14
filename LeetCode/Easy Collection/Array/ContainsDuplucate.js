f548f8bba25358caf99a83c7fff0d713c618b34f
oouuyab <01073785645@naver.com>
2020-08-14 16:30:09 +0900

    :heavy_plus_sign: Add ContainsDuplicate.js, IntersectionOfTwoArraysII.js and RotateArray.js

---
+++ LeetCode/Easy Collection/Array/ContainsDuplucate.js
@@ -0,0 +1,42 @@
+/*
+Contains Duplicate
+
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    // while(nums.length > 0){
+    //     let target = nums.shift();
+    //     if(nums.indexOf(target) > -1){
+    //         return true;
+    //     }
+    // }
+    // return false;
+
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            return true;
+        }
+    }
+    return false;
+};
