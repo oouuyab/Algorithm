1b5fe45b37d604b391db7391711531c18c38c836
oouuyab <01073785645@naver.com>
2020-09-29 20:12:05 +0900

    :heavy_plus_sign: Add SQL SUM, MAX, MIN & GROUP BY

---
+++ DataStructure/Queue.js
@@ -0,0 +1,11 @@
+let Queue = () => {
+  Queue = () => {
+    this.count = 0;
+    this.head = null;
+    this.rear = null;
+  };
+  Node = (data) => {
+    this.data = data;
+    this.next = null;
+  };
+};
